51d96ad24ba9435fdcc7be6fe13d037270c1b71a
shruti <shrutimittal1314@gmail.com>
2026-04-30 23:12:31 +0530

    final commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://team-task-manager-backend-production-2da3.up.railway.app',
 });
 
 api.interceptors.request.use(
